9f41ff58219a5acb76fd2cd9e7c33385144e65ca
Yang Yu <yy2623@columbia.edu>
2021-01-10 12:42:01 +0900

    Update helper script

--- utils/cutset_to_tsv.py
+++ utils/cutset_to_tsv.py
@@ -8,12 +8,17 @@
 
 import argparse
 import codecs
-from distutils.util import strtobool
-from lhotse import CutSet
 import os
 import re
-import sentencepiece as spm
 from tqdm import tqdm
+from lhotse import CutSet
+import sentencepiece as spm
+from distutils.util import strtobool
+from functools import partial, reduce
+from lhotse import NumpyFilesReader
+from lhotse.manipulation import combine
+from lhotse.utils import compute_num_frames
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cutset_yaml', type=str, default='', nargs='?',
@@ -35,12 +40,26 @@ parser.add_argument('--wp_model', type=str, default=False, nargs='?',
                     help='prefix of the wordpiece model')
 parser.add_argument('--wp_nbest', type=int, default=1, nargs='?',
                     help='')
+parser.add_argument('--tsv_location', type=str, default="",
+                    help='output location for tsv file')
+parser.add_argument('--jobs', type=int, default=10,
+                    help='number of jobs to process feature in parallel')
+parser.add_argument('--parallel', type=str, default="process",
+                    choices=['process', 'thread'],
+                    help='choice of parallelize execution of a Python method')
 args = parser.parse_args()
 
 
 def main():    
     
     cutset = CutSet.from_yaml(args.cutset_yaml)
+    
+    if args.parallel == "process":
+        from concurrent.futures import ProcessPoolExecutor
+        ex = ProcessPoolExecutor(args.jobs)
+    elif args.parallel == "thread":
+        from concurrent.futures import ThreadPoolExecutor
+        ex = ThreadPoolExecutor(args.jobs)
     
     nlsyms = []
     if args.nlsyms:
@@ -50,6 +69,8 @@ def main():
 
     token2idx = {}
     idx2token = {}
+    sp = None
+    
     with codecs.open(args.dict, 'r', encoding="utf-8") as f:
         for line in f:
             token, idx = line.strip().split(' ')
@@ -59,112 +80,137 @@ def main():
     if args.unit == 'wp':
         sp = spm.SentencePieceProcessor()
         sp.Load(args.wp_model + '.model')
+        
 
-    print('utt_id\tspeaker\tfeat_path\txlen\txdim\ttext\ttoken_id\tylen\tydim\tcut_id\tprev_utt')
-
-    xdim = None
-    pbar = tqdm(total=len(cutset))
-    utt2featpath = {}
-    utt2num_frames = {}
-    utt2spk = {}
-    for cut in cutset:
-        # Remove successive spaces
-        cut_id = cut.id
-        utt_id = cut.supervisions[0].id
-        feat_path = os.path.join(cut.features.storage_path, cut.features.storage_key)        
-        xlen,xdim = cut.load_features().shape
-        speaker = cut.supervisions[0].speaker
-
-        line = cut.supervisions[0].text
-        line = re.sub(r'[\s]+', ' ', line.strip())
-        words = line.split(' ')
-        if '' in words:
-            words.remove('')
-        text = ' '.join(words)
-
-        # Skip empty line
-        if text == '':
+    print_lines = ['utt_id\tspeaker\tfeat_path\txlen\txdim\ttext\ttoken_id\tylen\tydim\toffset\tprev_utt']
+    
+    futures = [
+        ex.submit(
+            Cut2Tsv,
+            cs,
+            token2idx,
+            idx2token,
+            nlsyms,
+            sp, 
+            args
+        )
+        for i, cs in enumerate(cutset)
+    ]
+    
+    progress = partial(
+            tqdm, desc='Converting cutset into tsv data', total=len(futures)
+        )
+    
+    print_lines += combine(progress(f.result() for f in futures))
+    
+    out = open(args.tsv_location, "w")
+    for line in print_lines:
+        if line == "":
             continue
+        out.write(line + "\n")
+    out.close()
 
-        # Convert strings into the corresponding indices
-        token_ids = []
-        if args.unit in ['word', 'word_char']:
-            for w in words:
-                if w in token2idx.keys():
-                    token_ids.append(token2idx[w])
-                else:
-                    # Replace with <unk>
-                    if args.unit == 'word_char':
-                        for c in list(w):
-                            if c in token2idx.keys():
-                                token_ids.append(token2idx[c])
-                            else:
-                                token_ids.append(token2idx[args.unk])
-                    else:
-                        token_ids.append(token2idx[args.unk])
+def Cut2Tsv(cut, token2idx, idx2token, nlsyms, sp, args):
+    cut_id = cut.id
+    utt_id = cut.supervisions[0].id
+    feat_path = cut.features.storage_path  + "@" + cut.features.storage_key
+    xlen,xdim = cut.load_features().shape
+    speaker = cut.supervisions[0].speaker
+    
+    offset = cut.start
+    duration = cut.duration
+    full_duration= cut.features.duration
+    num_frames = cut.features.num_frames
+    frame_shift = round(full_duration / num_frames, ndigits=3)
+    left_offset_frames = compute_num_frames(offset, frame_shift)
+    right_offset_frames = left_offset_frames + compute_num_frames(duration, frame_shift)
 
-        elif args.unit == 'wp':
-            # Remove space before the first special symbol
-            wps = sp.EncodeAsPieces(text)
-            if wps[0] == '▁' and wps[1][0] == '<':
-                wps = wps[1:]
+    line = cut.supervisions[0].text
+    line = re.sub(r'[\s]+', ' ', line.strip())
+    words = line.split(' ')
+    if '' in words:
+        words.remove('')
+    text = ' '.join(words)
 
-            for wp in wps:
-                if wp in token2idx.keys():
-                    token_ids.append(token2idx[wp])
-                else:
-                    # Replace with <unk>
-                    token_ids.append(token2idx[args.unk])
+    # Skip empty line
+    if text == '':
+        return [""]
 
-        elif args.unit == 'char':
-            for i, w in enumerate(words):
-                if w in nlsyms:
-                    token_ids.append(token2idx[w])
-                else:
+    # Convert strings into the corresponding indices
+    token_ids = []
+    if args.unit in ['word', 'word_char']:
+        for w in words:
+            if w in token2idx.keys():
+                token_ids.append(token2idx[w])
+            else:
+                # Replace with <unk>
+                if args.unit == 'word_char':
                     for c in list(w):
                         if c in token2idx.keys():
                             token_ids.append(token2idx[c])
                         else:
-                            # Replace with <unk>
                             token_ids.append(token2idx[args.unk])
-
-                # Insert <space> mark
-                if not args.remove_space:
-                    if i < len(words) - 1:
-                        token_ids.append(token2idx[args.space])
-
-        elif args.unit == 'phone':
-            for p in words:
-                token_ids.append(token2idx[p])
-
-        else:
-            raise ValueError(args.unit)
-        token_id = ' '.join(token_ids)
-        ylen = len(token_ids)
-        ydim = len(token2idx.keys())
-
-        print('%s\t%s\t%s\t%d\t%d\t%s\t%s\t%d\t%d\t%s' %
-              (utt_id, speaker, feat_path, xlen, xdim, text, token_id, ylen, ydim, cut_id))
-
-        # data augmentation for wordpiece
-        if args.unit == 'wp' and args.wp_nbest > 1:
-            raise NotImplementedError
-
-            for wp_i in sp.NBestEncodeAsPieces(text, args.wp_nbest)[1:]:
-                if wp_i in token2idx.keys():
-                    token_ids = token2idx[wp_i]
                 else:
-                    # Replace with <unk>
-                    token_ids = token2idx[args.unk]
+                    token_ids.append(token2idx[args.unk])
 
-                token_id = ' '.join(token_ids)
-                ylen = len(token_ids)
+    elif args.unit == 'wp':
+        # Remove space before the first special symbol
+        wps = sp.EncodeAsPieces(text)
+        if wps[0] == '▁' and wps[1][0] == '<':
+            wps = wps[1:]
 
-                print('%s\t%s\t%s\t%d\t%d\t%s\t%s\t%d\t%d\t%s' %
-                      (utt_id, speaker, feat_path, xlen, xdim, text, token_id, ylen, ydim, cut_id))
+        for wp in wps:
+            if wp in token2idx.keys():
+                token_ids.append(token2idx[wp])
+            else:
+                # Replace with <unk>
+                token_ids.append(token2idx[args.unk])
 
-        pbar.update(1)
+    elif args.unit == 'char':
+        for i, w in enumerate(words):
+            if w in nlsyms:
+                token_ids.append(token2idx[w])
+            else:
+                for c in list(w):
+                    if c in token2idx.keys():
+                        token_ids.append(token2idx[c])
+                    else:
+                        # Replace with <unk>
+                        token_ids.append(token2idx[args.unk])
 
+            # Insert <space> mark
+            if not args.remove_space:
+                if i < len(words) - 1:
+                    token_ids.append(token2idx[args.space])
+
+    elif args.unit == 'phone':
+        for p in words:
+            token_ids.append(token2idx[p])
+
+    else:
+        raise ValueError(args.unit)
+    token_id = ' '.join(token_ids)
+    ylen = len(token_ids)
+    ydim = len(token2idx.keys())
+
+    print_line = '%s\t%s\t%s\t%d\t%d\t%s\t%s\t%d\t%d\t%d'%(utt_id, speaker, feat_path, xlen, xdim, text, token_id, ylen, ydim, left_offset_frames)
+
+    # data augmentation for wordpiece
+    if args.unit == 'wp' and args.wp_nbest > 1:
+        raise NotImplementedError
+
+        for wp_i in sp.NBestEncodeAsPieces(text, args.wp_nbest)[1:]:
+            if wp_i in token2idx.keys():
+                token_ids = token2idx[wp_i]
+            else:
+                # Replace with <unk>
+                token_ids = token2idx[args.unk]
+
+            token_id = ' '.join(token_ids)
+            ylen = len(token_ids)
+
+            print_line = '%s\t%s\t%s\t%d\t%d\t%s\t%s\t%d\t%d\t%d\t%d'%(utt_id, speaker, feat_path, xlen, xdim, text, token_id, ylen, ydim, left_offset_frames, right_offset_frames)
+    return [print_line]
 
 if __name__ == '__main__':
     main()
--- utils/extract_feats.py
+++ utils/extract_feats.py
@@ -6,26 +6,17 @@
 
 """extract feats using lhotse"""
 
+import pickle
+import lilcom
 import argparse
-from distutils.util import strtobool
-from lhotse.kaldi import load_kaldi_data_dir
-from lhotse.features.io import LilcomFilesWriter
-from lhotse.dataset import SpeechRecognitionDataset
-from lhotse import CutSet, LilcomFilesWriter, FeatureSetBuilder, FeatureSet, RecordingSet, SupervisionSet
-from lhotse.dataset.speech_recognition import K2DataLoader, K2SpeechRecognitionDataset, \
-    K2SpeechRecognitionIterableDataset
-from lhotse.dataset.unsupervised import UnsupervisedDataset, UnsupervisedWaveformDataset
-from lhotse.dataset.vad import VadDataset
-from lhotse.dataset.diarization import DiarizationDataset
-from lhotse import Fbank, Mfcc, Spectrogram
-from lhotse import features
-import subprocess
-import torch
-import matplotlib.pyplot as plt
-from typing import Dict
-import numpy as np
-import sys
 import warnings
+from pathlib import Path
+from tqdm.auto import tqdm
+from functools import partial, reduce
+from lhotse.manipulation import combine
+from lhotse.kaldi import load_kaldi_data_dir
+from lhotse import CutSet, features, LilcomFilesWriter, FeatureSetBuilder
+
 def fxn():
     warnings.warn("deprecated", DeprecationWarning)
 
@@ -41,24 +32,41 @@ parser.add_argument('--sample_rate', type=int, default=16000,
 parser.add_argument('--feature_type', type=str, default=None,
                     choices=['fbank', 'spectrogram', 'mfcc'],
                     help='type of extracted feature, only valid when input_type is speech')
+parser.add_argument('--storage_type', type=str, default="Lilcom",
+                    choices=['Lilcom', 'Numpy'],
+                    help='feature storage storge')
 parser.add_argument('--cmvn', type=str, default=None,
-                    choices=['global', 'perinstance', 'online'],
-                    help='type of extracted feature, only valid when input_type is speech')
+                    choices=['Save_Apply', 'Load_Apply'],
+                    help='Calculate/Save or Load cmvn stats and apply')
+parser.add_argument('--cmvn_location', type=str, default=None,
+                    help='Location to load or save cmvn stats.')
+parser.add_argument('--parallel', type=str, default="process",
+                    choices=['process', 'thread'],
+                    help='choice of parallelize execution of a Python method')
+parser.add_argument('--speed_perturb', action='store_true',
+                    help='data will be augmented with speed perturbation 0.9 abd 1.1')
 parser.add_argument('--feature_config', type=str, default=False,
                     help='config file for lhotse feature extraction')
 parser.add_argument('--feature_dump_location', type=str, default=False,
                     help='path to dump extracted features')
 parser.add_argument('--cutset_yaml', type=str, default=False,
                     help='path to cutset yaml file')
-parser.add_argument('--jobs', type=int, default=50,
+parser.add_argument('--jobs', type=int, default=10,
                     help='number of jobs to process feature in parallel')
+
 parser.add_argument("-f", "--fff", help="a dummy argument to fool ipython", default="1")
 args = parser.parse_args()
 
 def main():
     
-    recording_set, supervision_set= load_kaldi_data_dir(args.data, args.sample_rate)
+    recording_set, supervision_set= load_kaldi_data_dir(args.data, args.sample_rate)    
     print("Done Generating Recording and Supervision manifest from (%s)"%args.data)
+    
+    cut_set = CutSet.from_manifests(recordings=recording_set, supervisions=supervision_set)
+    if (args.speed_perturb):
+        cut_set = cut_set + cut_set.perturb_speed(0.9) + cut_set.perturb_speed(1.1)
+
+    jobs = min(len(cut_set), args.jobs)
 
     if args.feature_type == None:
         print("Based on the settings, features are not extracted for dataset %s"%dataset)\
@@ -73,28 +81,90 @@ def main():
         print("%s is not a valid option for feature. Fbank is picked as feature type.")
         from lhotse import Fbank as feature_pipeline
 
+    if args.parallel == "process":
+        from concurrent.futures import ProcessPoolExecutor
+        ex = ProcessPoolExecutor(jobs)
+    elif args.parallel == "thread":
+        from concurrent.futures import ThreadPoolExecutor
+        ex = ThreadPoolExecutor(jobs)
+        
+    if args.storage_type == "Lilcom":
+        from lhotse.features.io import LilcomFilesWriter as storage_type
+        import lilcom
+    elif args.storage_type == "Numpy":
+        from lhotse.features.io import NumpyFilesWriter as storage_type
+        import numpy as np
+
+        
     if args.feature_config == "":
         feature_extractor = feature_pipeline()
     else:
         feature_extractor = feature_pipeline.from_yaml(args.feature_config)
+
     
-    # Create a feature set builder that uses this extractor and dumps the results in specified location
-    with LilcomFilesWriter(args.feature_dump_location) as storage:
-        builder = FeatureSetBuilder(feature_extractor=feature_extractor, storage=storage)
-        # Extract the features using 8 parallel processes, compress, and store them on in 'features/storage/' directory.
-        # Then, return the feature manifest object, which is also compressed and
-        # stored in 'features/feature_manifest.json.gz'
-        feature_set = builder.process_and_store_recordings(
-            recordings=recording_set,
-            num_jobs=args.jobs
-        )
+        
+    cut_set = cut_set.compute_and_store_features(
+                extractor=feature_extractor,
+                storage_path=args.feature_dump_location,
+                storage_type=storage_type,
+                executor=ex,
+                num_jobs=jobs
+            )
     print("Done Extracting feature in type (%s)"%(args.feature_type))
     
-    cut_set = CutSet.from_manifests(recordings=recording_set, features=feature_set, supervisions=supervision_set)
+    if ("Save" in args.cmvn):
+        stats = cut_set.compute_global_feature_stats(storage_path = args.cmvn_location)
+        print("Done Computing CMVN stats from this feature set")
+    elif ("Load" in args.cmvn):
+        with open(args.cmvn_location, 'rb') as f:
+             stats=pickle.load(f)
+    
+    if ("Apply" in args.cmvn):
+        futures = [
+            ex.submit(
+                Apply_cmvn_on_cut,
+                cs,
+                stats,
+                args.storage_type
+            )
+            for i, cs in enumerate(cut_set)
+        ]
+        
+        progress = partial(
+                tqdm, desc='Applying CMVN on extacted features (chunks progress)', total=len(futures)
+            )
+        
+        cut_set = combine(progress(f.result() for f in futures))
+
+    if args.parallel == "process":
+        ex = ProcessPoolExecutor(args.jobs)
+    elif args.parallel == "thread":
+        ex = ThreadPoolExecutor(args.jobs)
+
     cut_set = cut_set.trim_to_supervisions()
     cut_set.to_yaml(args.cutset_yaml)
 
     print("Done Generating Cut manifests save at (%s)"%args.cutset_yaml)
 
+def Apply_cmvn_on_cut(cut, stats, storage_type):
+    feature = cut.load_features()
+    feature_cmvn = (feature - stats["norm_means"])/stats["norm_stds"]
+    storage_key = cut.features.storage_key[4:]
+    new_storage_path_ = Path(cut.features.storage_path+"_cmvn")
+    new_storage_path_.mkdir(parents=True, exist_ok=True)
+    subdir = new_storage_path_ / storage_key[:3]
+    subdir.mkdir(exist_ok=True)
+    if storage_type == "Lilcom":
+        output_features_path = (subdir / storage_key).with_suffix('.llc')
+        serialized_feats = lilcom.compress(feature_cmvn, tick_power=-5)
+        with open(output_features_path, 'wb') as f:
+            f.write(serialized_feats)
+    elif storage_type == "Numpy":
+        output_features_path = (subdir / storage_key).with_suffix('.npy')
+        np.save(output_features_path, feature_cmvn, allow_pickle=False)
+    cut.features.storage_path = cut.features.storage_path+"_cmvn"
+    return CutSet.from_cuts([cut])
+    
+    
 if __name__ == '__main__':
     main()
